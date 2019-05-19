4f96ac2bc976bee681a15956317d682181abb815
yuxiqian <akaza_akari@sjtu.edu.cn>
2019-05-19 13:55:57 +0800

    [add] powershell initializer for centOS

---
+++ ps-init/initialize_powershell.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash 
+
+# Update the list of products
+sudo apt-get update
+
+# Register the Microsoft RedHat repository
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+
+# Install PowerShell
+sudo yum install -y powershell
+
+# Start PowerShell
+pwsh
